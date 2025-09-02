11f2c1105092ee2fc6e71d0d5a7b07b0e87aae75
Kim Roberts <kimr@apple.com>
2025-09-01 15:48:25 -1000

    fix: handling local (postgres) and prod (neon) db

--- src/lib/db/server/getDb.server.ts
+++ src/lib/db/server/getDb.server.ts
@@ -1,8 +1,10 @@
 import { neon } from '@neondatabase/serverless'
 import debug from 'debug'
-import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
-import { drizzle as drizzlePostgresJs } from 'drizzle-orm/postgres-js'
+import { drizzle as drizzleNeon, NeonHttpDatabase } from 'drizzle-orm/neon-http'
+import { drizzle as drizzlePostgresJs, PostgresJsDatabase } from 'drizzle-orm/postgres-js'
 import * as schema from '../tables'
+
+type DB = NeonHttpDatabase<typeof schema> | PostgresJsDatabase<typeof schema>
 
 const log = debug('grocery-program:getDb')
 const { DATABASE_LOG, DATABASE_URL } = process.env
@@ -13,17 +15,20 @@ if (!DATABASE_URL) {
 
 log(`connected to ${DATABASE_URL}`)
 
+let db: DB
 const isLocal = DATABASE_URL.includes('localhost')
-const config = {
-    connection: {
-        url: DATABASE_URL,
-    },
-    schema,
-    logger: DATABASE_LOG === 'true',
+if (isLocal) {
+    db = drizzlePostgresJs({
+        connection: {
+            url: DATABASE_URL,
+        },
+        schema,
+        logger: DATABASE_LOG === 'true',
+    })
+} else {
+    const sql = neon(DATABASE_URL)
+    db = drizzleNeon(sql, { schema })
 }
-
-const sql = neon(DATABASE_URL)
-const db = isLocal ? drizzlePostgresJs(config) : drizzleNeon(sql, { schema })
 
 export function getDb() {
     return db
