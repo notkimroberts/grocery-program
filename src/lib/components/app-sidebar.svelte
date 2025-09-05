f296c59d2672a698b252ab5acb2b6e96c3f226e1
Kim Roberts <kimr@apple.com>
2025-09-05 08:24:29 -1000

    fix: update sidebar

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,203 +1,171 @@
 <script lang="ts" module>
-    const data = {
-        navMain: [
-            {
-                title: 'Getting Started',
-                url: '#',
-                items: [
-                    {
-                        title: 'Installation',
-                        url: '#',
-                    },
-                    {
-                        title: 'Project Structure',
-                        url: '#',
-                    },
-                ],
-            },
-            {
-                title: 'Building Your Application',
-                url: '#',
-                items: [
-                    {
-                        title: 'Routing',
-                        url: '#',
-                    },
-                    {
-                        title: 'Data Fetching',
-                        url: '#',
-                        isActive: true,
-                    },
-                    {
-                        title: 'Rendering',
-                        url: '#',
-                    },
-                    {
-                        title: 'Caching',
-                        url: '#',
-                    },
-                    {
-                        title: 'Styling',
-                        url: '#',
-                    },
-                    {
-                        title: 'Optimizing',
-                        url: '#',
-                    },
-                    {
-                        title: 'Configuring',
-                        url: '#',
-                    },
-                    {
-                        title: 'Testing',
-                        url: '#',
-                    },
-                    {
-                        title: 'Authentication',
-                        url: '#',
-                    },
-                    {
-                        title: 'Deploying',
-                        url: '#',
-                    },
-                    {
-                        title: 'Upgrading',
-                        url: '#',
-                    },
-                    {
-                        title: 'Examples',
-                        url: '#',
-                    },
-                ],
-            },
-            {
-                title: 'API Reference',
-                url: '#',
-                items: [
-                    {
-                        title: 'Components',
-                        url: '#',
-                    },
-                    {
-                        title: 'File Conventions',
-                        url: '#',
-                    },
-                    {
-                        title: 'Functions',
-                        url: '#',
-                    },
-                    {
-                        title: 'next.config.js Options',
-                        url: '#',
-                    },
-                    {
-                        title: 'CLI',
-                        url: '#',
-                    },
-                    {
-                        title: 'Edge Runtime',
-                        url: '#',
-                    },
-                ],
-            },
-            {
-                title: 'Architecture',
-                url: '#',
-                items: [
-                    {
-                        title: 'Accessibility',
-                        url: '#',
-                    },
-                    {
-                        title: 'Fast Refresh',
-                        url: '#',
-                    },
-                    {
-                        title: 'Next.js Compiler',
-                        url: '#',
-                    },
-                    {
-                        title: 'Supported Browsers',
-                        url: '#',
-                    },
-                    {
-                        title: 'Turbopack',
-                        url: '#',
-                    },
-                ],
-            },
-            {
-                title: 'Community',
-                url: '#',
-                items: [
-                    {
-                        title: 'Contribution Guide',
-                        url: '#',
-                    },
-                ],
-            },
-        ],
-    }
+	import AudioWaveformIcon from "@lucide/svelte/icons/audio-waveform";
+	import BookOpenIcon from "@lucide/svelte/icons/book-open";
+	import BotIcon from "@lucide/svelte/icons/bot";
+	import ChartPieIcon from "@lucide/svelte/icons/chart-pie";
+	import CommandIcon from "@lucide/svelte/icons/command";
+	import FrameIcon from "@lucide/svelte/icons/frame";
+	import GalleryVerticalEndIcon from "@lucide/svelte/icons/gallery-vertical-end";
+	import MapIcon from "@lucide/svelte/icons/map";
+	import Settings2Icon from "@lucide/svelte/icons/settings-2";
+	import SquareTerminalIcon from "@lucide/svelte/icons/square-terminal";
+
+	// This is sample data.
+	const data = {
+		user: {
+			name: "shadcn",
+			email: "m@example.com",
+			avatar: "/avatars/shadcn.jpg",
+		},
+		teams: [
+			{
+				name: "Acme Inc",
+				logo: GalleryVerticalEndIcon,
+				plan: "Enterprise",
+			},
+			{
+				name: "Acme Corp.",
+				logo: AudioWaveformIcon,
+				plan: "Startup",
+			},
+			{
+				name: "Evil Corp.",
+				logo: CommandIcon,
+				plan: "Free",
+			},
+		],
+		navMain: [
+			{
+				title: "Playground",
+				url: "#",
+				icon: SquareTerminalIcon,
+				isActive: true,
+				items: [
+					{
+						title: "History",
+						url: "#",
+					},
+					{
+						title: "Starred",
+						url: "#",
+					},
+					{
+						title: "Settings",
+						url: "#",
+					},
+				],
+			},
+			{
+				title: "Models",
+				url: "#",
+				icon: BotIcon,
+				items: [
+					{
+						title: "Genesis",
+						url: "#",
+					},
+					{
+						title: "Explorer",
+						url: "#",
+					},
+					{
+						title: "Quantum",
+						url: "#",
+					},
+				],
+			},
+			{
+				title: "Documentation",
+				url: "#",
+				icon: BookOpenIcon,
+				items: [
+					{
+						title: "Introduction",
+						url: "#",
+					},
+					{
+						title: "Get Started",
+						url: "#",
+					},
+					{
+						title: "Tutorials",
+						url: "#",
+					},
+					{
+						title: "Changelog",
+						url: "#",
+					},
+				],
+			},
+			{
+				title: "Settings",
+				url: "#",
+				icon: Settings2Icon,
+				items: [
+					{
+						title: "General",
+						url: "#",
+					},
+					{
+						title: "Team",
+						url: "#",
+					},
+					{
+						title: "Billing",
+						url: "#",
+					},
+					{
+						title: "Limits",
+						url: "#",
+					},
+				],
+			},
+		],
+		projects: [
+			{
+				name: "Design Engineering",
+				url: "#",
+				icon: FrameIcon,
+			},
+			{
+				name: "Sales & Marketing",
+				url: "#",
+				icon: ChartPieIcon,
+			},
+			{
+				name: "Travel",
+				url: "#",
+				icon: MapIcon,
+			},
+		],
+	};
 </script>
 
 <script lang="ts">
-    import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end'
-    import type { ComponentProps } from 'svelte'
-    import * as Sidebar from '$lib/components/ui/sidebar/index.js'
+	import NavMain from "./nav-main.svelte";
+	import NavProjects from "./nav-projects.svelte";
+	import NavUser from "./nav-user.svelte";
+	import TeamSwitcher from "./team-switcher.svelte";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import type { ComponentProps } from "svelte";
 
-    let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props()
+	let {
+		ref = $bindable(null),
+		collapsible = "icon",
+		...restProps
+	}: ComponentProps<typeof Sidebar.Root> = $props();
 </script>
 
-<Sidebar.Root {...restProps} bind:ref>
-    <Sidebar.Header>
-        <Sidebar.Menu>
-            <Sidebar.MenuItem>
-                <Sidebar.MenuButton size="lg">
-                    {#snippet child({ props })}
-                        <a href="##" {...props}>
-                            <div
-                                class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                                <GalleryVerticalEndIcon class="size-4" />
-                            </div>
-                            <div class="flex flex-col gap-0.5 leading-none">
-                                <span class="font-medium">Documentation</span>
-                                <span class="">v1.0.0</span>
-                            </div>
-                        </a>
-                    {/snippet}
-                </Sidebar.MenuButton>
-            </Sidebar.MenuItem>
-        </Sidebar.Menu>
-    </Sidebar.Header>
-    <Sidebar.Content>
-        <Sidebar.Group>
-            <Sidebar.Menu>
-                {#each data.navMain as item (item.title)}
-                    <Sidebar.MenuItem>
-                        <Sidebar.MenuButton class="font-medium">
-                            {#snippet child({ props })}
-                                <a href={item.url} {...props}>
-                                    {item.title}
-                                </a>
-                            {/snippet}
-                        </Sidebar.MenuButton>
-                        {#if item.items?.length}
-                            <Sidebar.MenuSub>
-                                {#each item.items as subItem (subItem.title)}
-                                    <Sidebar.MenuSubItem>
-                                        <Sidebar.MenuSubButton isActive={subItem.isActive}>
-                                            {#snippet child({ props })}
-                                                <a href={subItem.url} {...props}>{subItem.title}</a>
-                                            {/snippet}
-                                        </Sidebar.MenuSubButton>
-                                    </Sidebar.MenuSubItem>
-                                {/each}
-                            </Sidebar.MenuSub>
-                        {/if}
-                    </Sidebar.MenuItem>
-                {/each}
-            </Sidebar.Menu>
-        </Sidebar.Group>
-    </Sidebar.Content>
-    <Sidebar.Rail />
+<Sidebar.Root {collapsible} {...restProps}>
+	<Sidebar.Header>
+		<TeamSwitcher teams={data.teams} />
+	</Sidebar.Header>
+	<Sidebar.Content>
+		<NavMain items={data.navMain} />
+		<NavProjects projects={data.projects} />
+	</Sidebar.Content>
+	<Sidebar.Footer>
+		<NavUser user={data.user} />
+	</Sidebar.Footer>
+	<Sidebar.Rail />
 </Sidebar.Root>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -16,20 +16,23 @@
 <Sidebar.Provider>
     <AppSidebar />
     <Sidebar.Inset>
-        <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-            <Sidebar.Trigger class="-ml-1" />
-            <Separator orientation="vertical" class="mr-2 h-4" />
-            <Breadcrumb.Root>
-                <Breadcrumb.List>
-                    <Breadcrumb.Item class="hidden md:block">
-                        <Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
-                    </Breadcrumb.Item>
-                    <Breadcrumb.Separator class="hidden md:block" />
-                    <Breadcrumb.Item>
-                        <Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-                    </Breadcrumb.Item>
-                </Breadcrumb.List>
-            </Breadcrumb.Root>
+        <header
+            class="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+            <div class="flex items-center gap-2 px-4">
+                <Sidebar.Trigger class="-ml-1" />
+                <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+                <Breadcrumb.Root>
+                    <Breadcrumb.List>
+                        <Breadcrumb.Item class="hidden md:block">
+                            <Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
+                        </Breadcrumb.Item>
+                        <Breadcrumb.Separator class="hidden md:block" />
+                        <Breadcrumb.Item>
+                            <Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+                        </Breadcrumb.Item>
+                    </Breadcrumb.List>
+                </Breadcrumb.Root>
+            </div>
         </header>
         <div>
             <main>
